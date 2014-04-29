b1627a5deae3ec6d5e96295a27356bda506b8f70
ruby -v <leo_mejia_p@hotmail.com>
2014-04-29 13:43:17 -0400

    Subiendo Proyecto Domotica a Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DomoticaProyect::Application.config.secret_key_base = '6e94a42287b1a45c6083c905bd1576b57207facdf412440e25940e31b9047cbd1b55af21ddbb928ae3f732f1dab2d609114e13b5f6009cc551875db8f5855cfe'
