dbdeb51c53ec4c4833f1e8d01b0d55e511ae1b78
ruby -v <leo_mejia_p@hotmail.com>
2014-05-20 11:04:13 -0400

    agregando modificaciones usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515151751) do
+ActiveRecord::Schema.define(version: 20140520141521) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
