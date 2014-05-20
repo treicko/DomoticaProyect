58602c9a8b33e41f037633578c33970a29951d19
ruby -v <leo_mejia_p@hotmail.com>
2014-05-20 08:09:16 -0400

    resolviendo error en roify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   rolify
-  resourcify
+  #resourcify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :locations, :dependent => :destroy
   has_many :termometros, :through => :locations, :source => :thermostats 
+
+  def self.with_role(role)
+     my_role = Role.find_by_name(role)
+     where(:role => my_role)
+  end
 end
