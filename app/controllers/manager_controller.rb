fee504f06a3798374ae40c34607dc3c6746e3d06
Jorge <jorge@jorge-Inspiron-N5110.(none)>
2014-05-20 22:01:05 -0400

    Arreglando autentificacion

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,6 +1,5 @@
 class ManagerController < ApplicationController
     #load_and_authorize_resource :only => [:users_list, :enable_user, :deshabilitar_usuario] 
-	before_action :authenticate_user!
 
 	def users_list
 		@users = User.all
