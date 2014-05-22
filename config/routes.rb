71021d30ad5bff1d6332e7e4d723df8cdb417e14
ruby -v <leo_mejia_p@hotmail.com>
2014-05-22 07:14:33 -0400

    agregando toda mi funcionalidad

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ DomoticaProyect::Application.routes.draw do
 
   resources :clients
 
-  devise_for :users
+  #devise_for :users
+  devise_for :users #, :skip => [:sessions]
+    #as :user do
+      #get 'signin' => 'devise/sessions#new', :as => :new_user_session
+      #post 'signin' => 'devise/sessions#create', :as => :user_session
+      #delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+      #match 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session,
+      #:via => Devise.mappings[:user].sign_out_via
+    #end
+
   root 'principals#index'
 
   get '/manager/users_list' => 'manager#users_list'
