5a51fe51c293cc56a15ac3708594b74a9bdac5c3
ruby -v <leo_mejia_p@hotmail.com>
2014-05-31 15:08:07 -0400

    subiendo arreglos merge

--- app/models/thermostat_history.rb
+++ app/models/thermostat_history.rb
@@ -1,6 +1,6 @@
 class ThermostatHistory < ActiveRecord::Base
 	belongs_to :thermostat
-	validate :existence_of_thermostat, :correct_user
+	validate :existence_of_thermostat#, :correct_user
 	def existence_of_thermostat
 		if thermostat==nil
 			errors.add(:thermostat, "can't be nil")
