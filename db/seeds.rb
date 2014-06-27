520561369ae67cda464e40f6c53272e5a2129730
ruby -v <leo_mejia_p@hotmail.com>
2014-06-26 21:05:50 -0400

    agregando valores

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.new({:email => "osvaldo@hotmail.com", :password => "123456789", :password_confirmation => "123456789", :rol =>"Usuario", :user_enable => true}).save(:validate =>false)
+User.new({:email => "osvaldo@hotmail.com", :password => "12341234", :password_confirmation => "12341234", :rol =>"Usuario", :user_enable => true}).save(:validate =>false)
 User.new({:email => "admin@admin.com", :password => "12341234", :password_confirmation => "12341234", :rol =>"Administrador", :user_enable => true}).save(:validate =>false)
-Location.new({}).save
+Location.new({:address => "Casa", :user_id => 1, :country => "Bolivia", :region => "Cochabamba"}).save
+Thermostat.new({:serial_number => 1, :location_id => 1, :place => "Cuarto", :temperature => 15, :configuration => "true"}).save
+Thermostat.new({:serial_number => 2, :location_id => 1, :place => "Cocina", :temperature => 16, :configuration => "false"}).save
